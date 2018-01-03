4da2a48f5b742edc5f262e6476d4894a00762885
tianxiongbb <yutianxiong@gmail.com>
2018-01-03 16:11:11 -0500

    beta v1.2

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 PATH_PRO=$PWD"/"`dirname $0`
 echo -e "# added by piPipes_summary installer\nexport PATH=${PATH_PRO}:\$PATH" >> ~/.bashrc
-echo -e "export PYTHONPATH=${PATH_PRO}/lib_python:\t$PYTHONPATH" >> ~/.bashrc
+echo -e "export PYTHONPATH=${PATH_PRO}/lib_python:\$PYTHONPATH" >> ~/.bashrc
